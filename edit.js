20c8b79ba133af0d2bf4e316dc60e3a478f8dcd2
Tim Cameron Ryan <id@timryan.org>
2015-12-23 11:02:28 -0500

    Cleanup.

--- edit.js
+++ edit.js
@@ -4,11 +4,10 @@ var size = { x: 300, y: 100 };
 var renderer = PIXI.autoDetectRenderer(size.x, size.y);
 renderer.view.className = "rendererView";
 
-
 class CircleMesh extends PIXI.AbstractFilter {
-	constructor() {
-		var vertexShader = null;
-		var fragmentShader = `
+  constructor() {
+    var vertexShader = null;
+    var fragmentShader = `
 
 precision mediump float;
 
@@ -24,33 +23,39 @@ uniform sampler2D uSampler;
  */
 
 void main(void) {
-	vec2 pos = vTextureCoord*dimensions.xy;
-	vec2 center = dimensions.xy*0.5;
-	vec3 color = vec3(1.0, 1.0, 0);
-	float d = clamp(length(pos - center) - radius, 0.0, 2.0);
-    // if (d > 0.0 && d < 1.0)
-    // 	gl_FragColor = vec4(color*d, 1.0);
-   	// else if (d >= 1.0 && d < 2.0)
-    // 	gl_FragColor = vec4(color*(2.0-d), 1.0);
-    if (d > 0.0 && d <= 1.0)
-    	gl_FragColor = vec4(color, 1.0);
-    else
-    	gl_FragColor = vec4(0.0);
+  vec2 pos = vTextureCoord*dimensions.xy;
+  vec2 center = dimensions.xy*0.5;
+  vec3 color = vec3(1.0, 1.0, 0);
+  float d = clamp(length(pos - center) - radius, 0.0, 2.0);
+  
+  // Antialias
+  /*
+  if (d > 0.0 && d < 1.0)
+    gl_FragColor = vec4(color*d, 1.0);
+  else if (d >= 1.0 && d < 2.0)
+    gl_FragColor = vec4(color*(2.0-d), 1.0);
+  */
+
+  // Alias
+  if (d > 0.0 && d <= 1.0)
+    gl_FragColor = vec4(color, 1.0);
+  else
+    gl_FragColor = vec4(0.0);
 }
 
-		`;
+    `;
 
-		super(vertexShader, fragmentShader, {
-			dimensions: {
-      			type: '4fv',
-      			value: new Float32Array([0, 0, 0, 0])
-    		},
-    		radius: {
-    			type: 'f',
-    			value: 50.0,
-    		}
-		});
-	}
+    super(vertexShader, fragmentShader, {
+      dimensions: {
+          type: '4fv',
+          value: new Float32Array([0, 0, 0, 0])
+        },
+        radius: {
+          type: 'f',
+          value: 50.0,
+        }
+    });
+  }
 }
 
 
@@ -66,100 +71,19 @@ playpen.interactive = true;
 var circle = new CircleMesh;
 playpen.filters = [circle];
 
-playpen.beginFill( 0xFFFFFFFF );
+playpen.beginFill(0xffffffff);
 playpen.drawRect(0, 0, size.x, size.y);
 playpen.endFill();
 
-playpen.mousedown = playpen.touchstart = function(item){
- //  var coords = item.data.getLocalPosition( playpen );
-	// playpen.beginFill( 0xFFFFFFFF );
-	// console.log(coords)
-	// playpen.drawRect(Math.floor(coords.x), Math.floor(coords.y), 1, 1);
-	// playpen.endFill();
-	renderer.render(playpen);	
+playpen.mousedown = playpen.touchstart = function (item){
+  var coords = item.data.getLocalPosition(playpen);
+  renderer.render(playpen); 
 };
 
-playpen.mousemove = playpen.touchstart = function(item){
-	var coords = item.data.getLocalPosition( playpen );
-	circle.uniforms.radius.value = Math.sqrt(Math.pow(coords.x - size.x/2, 2) + Math.pow(coords.y - size.y/2, 2))
-	// playpen.beginFill( 0xFFFFFFFF );
-	// console.log(coords)
-	// playpen.drawRect(Math.floor(coords.x), Math.floor(coords.y), 1, 1);
-	// playpen.endFill();
-	renderer.render(playpen);	
+playpen.mousemove = playpen.touchstart = function (item){
+  var coords = item.data.getLocalPosition(playpen);
+  circle.uniforms.radius.value = Math.sqrt(Math.pow(coords.x - size.x/2, 2) + Math.pow(coords.y - size.y/2, 2))
+  renderer.render(playpen); 
 };
 
 renderer.render(playpen);
-// function ShockwaveFilter()
-// {
-//     core.AbstractFilter.call(this,
-//         // vertex shader
-//         null,
-//         // fragment shader
-//         fs.readFileSync(__dirname + '/shockwave.frag', 'utf8'),
-//         // custom uniforms
-//         {
-//             center: { type: 'v2', value: { x: 0.5, y: 0.5 } },
-//             params: { type: 'v3', value: { x: 10, y: 0.8, z: 0.1 } },
-//             time: { type: '1f', value: 0 }
-//         }
-//     );
-// }
-
-// ShockwaveFilter.prototype = Object.create(core.AbstractFilter.prototype);
-// ShockwaveFilter.prototype.constructor = ShockwaveFilter;
-// module.exports = ShockwaveFilter;
-
-// Object.defineProperties(ShockwaveFilter.prototype, {
-//     /**
-//      * Sets the center of the shockwave in normalized screen coords. That is
-//      * (0,0) is the top-left and (1,1) is the bottom right.
-//      *
-//      * @member {object<string, number>}
-//      * @memberof PIXI.filters.ShockwaveFilter#
-//      */
-//     center: {
-//         get: function ()
-//         {
-//             return this.uniforms.center.value;
-//         },
-//         set: function (value)
-//         {
-//             this.uniforms.center.value = value;
-//         }
-//     },
-//     /**
-//      * Sets the params of the shockwave. These modify the look and behavior of
-//      * the shockwave as it ripples out.
-//      *
-//      * @member {object<string, number>}
-//      * @memberof PIXI.filters.ShockwaveFilter#
-//      */
-//     params: {
-//         get: function ()
-//         {
-//             return this.uniforms.params.value;
-//         },
-//         set: function (value)
-//         {
-//             this.uniforms.params.value = value;
-//         }
-//     },
-//     /**
-//      * Sets the elapsed time of the shockwave. This controls the speed at which
-//      * the shockwave ripples out.
-//      *
-//      * @member {number}
-//      * @memberof PIXI.filters.ShockwaveFilter#
-//      */
-//     time: {
-//         get: function ()
-//         {
-//             return this.uniforms.time.value;
-//         },
-//         set: function (value)
-//         {
-//             this.uniforms.time.value = value;
-//         }
-//     }
-// });
