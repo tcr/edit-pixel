d57f1bfd6657a8396618f36dffb7943fa85b544b
Tim Cameron Ryan <id@timryan.org>
2016-01-03 02:27:10 -0500

    Enables hue-based color selection.

--- src/filters.js
+++ src/filters.js
@@ -96,3 +96,132 @@ void main(void) {
     });
   }
 }
+
+
+// HSB filter
+
+export class HFilter extends PIXI.AbstractFilter {
+  constructor() {
+    var fragmentShader = `
+
+precision mediump float;
+
+varying vec2 vTextureCoord;
+uniform vec4 dimensions;
+
+// http://gamedev.stackexchange.com/questions/59797/glsl-shader-change-hue-saturation-brightness
+
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main(void) {
+  gl_FragColor = vec4(hsv2rgb(vec3(vTextureCoord.y, 1.0, 1.0)), 1.0);
+}
+
+    `;
+
+    super(null, fragmentShader, {
+      dimensions: {
+        type: '4fv',
+        value: new Float32Array([0, 0, 0, 0])
+      },
+    });
+  }
+}
+
+export class SFilter extends PIXI.AbstractFilter {
+  constructor() {
+    var fragmentShader = `
+
+precision mediump float;
+
+varying vec2 vTextureCoord;
+uniform vec4 dimensions;
+
+// http://gamedev.stackexchange.com/questions/59797/glsl-shader-change-hue-saturation-brightness
+
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main(void) {
+  gl_FragColor = vec4(hsv2rgb(vec3(0.0, vTextureCoord.y, 0.75)), 1.0);
+}
+
+    `;
+
+    super(null, fragmentShader, {
+      dimensions: {
+        type: '4fv',
+        value: new Float32Array([0, 0, 0, 0])
+      },
+    });
+  }
+}
+
+export class BFilter extends PIXI.AbstractFilter {
+  constructor() {
+    var fragmentShader = `
+
+precision mediump float;
+
+varying vec2 vTextureCoord;
+uniform vec4 dimensions;
+
+// http://gamedev.stackexchange.com/questions/59797/glsl-shader-change-hue-saturation-brightness
+
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main(void) {
+  gl_FragColor = vec4(hsv2rgb(vec3(1.0, 1.0, vTextureCoord.y)), 1.0);
+}
+
+    `;
+
+    super(null, fragmentShader, {
+      dimensions: {
+        type: '4fv',
+        value: new Float32Array([0, 0, 0, 0])
+      },
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import shapes from './shapes';
-import { GridFilter } from './filters';
+import { GridFilter, HFilter, SFilter, BFilter } from './filters';
 import { calculateLine } from './util';
+
+import tinycolor from 'tinycolor2';
 
 var size = {x: 50, y: 50};
 var frameSize = {x: 300, y: 300};
@@ -10,22 +12,69 @@ var renderer = shapes.renderer;
 renderer.backgroundColor = 0xaaaaaa;
 $('#panel-graphics').append(renderer.view);
 
-renderer.view.style.height = '100%';
-renderer.view.style.width = '100%';
 
-var RADIUS_STATE = 0;
-var ALIAS_STATE = false;
-var ROT_BY = 0;
-var CIRC_DRAW = false;
-var CIRC_CENTER = {x: 0, y: 0};
-var CIRC_COLOR = '#000000';
-var ZOOM_VALUE = 4.0;
+var state;
+if (localStorage.state) {
+  try {
+    state = JSON.parse(localStorage.state);
+  } catch (e) { }
+}
+if (!state) {
+  state = {
+    brushSize: 0,
+    alias: false,
+    rotation: 0,
+    brushPainting: false,
+    brushColor: '#000000',
+    zoom: 4.0,
+    gridVisible: false,
+    cursorX: 0,
+    cursorY: 0,
+    image: null,
+  };
+}
+
+console.log('Initial state:', state);
+
+function saveGraphic () {
+  shapes.sprite.visible = false;
+  render();
+  state.image = bgcache.getBase64();
+  shapes.sprite.visible = true;
+  render();
+}
+
+function alterState (mod) {
+  for (var k in mod) {
+    if (!state.hasOwnProperty(k)) {
+      throw new Error('Key "' + k + '" missing');
+    }
+    state[k] = mod[k];
+  }
+  localStorage.state = JSON.stringify(state);
+}
 
 var background = new PIXI.Graphics();
 background.beginFill(0xffffff);
 background.drawRect(0, 0, size.x, size.y);
 
 var container = new PIXI.Container();
+
+if (state.image) {
+  try {
+    var tex = PIXI.Texture.fromImage(state.image);
+    tex.on('update', function () {
+      start();
+    })
+    background = new PIXI.Sprite(tex);
+  } catch (e) {
+    console.error(e.stack);
+    state.image = null;
+  }
+}
+if (!state.image) {
+  setTimeout(start);
+}
 container.addChild(background);
 
 var bgcache = new PIXI.RenderTexture(renderer, size.x, size.y, PIXI.SCALE_MODES.NEAREST);
@@ -47,13 +96,13 @@ var realContainer = new PIXI.Container();
 realContainer.addChild(bgcacheSprite);
 realContainer.scale.x = 1.0;
 realContainer.scale.y = 1.0;
-realContainer.position.x = 300 + -50;
-realContainer.position.y = 300 + -50;
+realContainer.position.x = frameSize.x/2 + -size.x/2;
+realContainer.position.y = frameSize.y/2 + -size.y/2;
 
 var lastCoords = null;
 
 function setZoom (value) {
-  ZOOM_VALUE = value;
+  alterState({ zoom: value });
   realContainer.scale.x = value;
   realContainer.scale.y = value;
   realContainer.position.x = frameSize.x/2 + -((size.x * value) / 2);
@@ -67,39 +116,41 @@ function setZoom (value) {
 }
 
 function resize () {
-  setZoom(ZOOM_VALUE);
+  setZoom(state.zoom);
 }
 
 function setBrushSize(value) {
-  RADIUS_STATE = value;
+  alterState({ brushSize: value });
   $('#toolconf-brush-size').val(value);
   $('#toolconf-brush-size-readout').text(value);
   render();
 }
 
 function render() {
-  // if (CIRC_DRAW) {
-    shapes.update(ALIAS_STATE, RADIUS_STATE, ROT_BY, CIRC_COLOR);
-    shapes.sprite.x = CIRC_CENTER.x - (shapes.sprite.width / 2);
-    shapes.sprite.y = CIRC_CENTER.y - (shapes.sprite.height / 2);
+  // if (state.brushPainting) {
+    shapes.update(state.alias, state.brushSize, state.rotation, state.brushColor);
+    shapes.sprite.x = state.cursorX - (shapes.sprite.width / 2);
+    shapes.sprite.y = state.cursorY - (shapes.sprite.height / 2);
   // }
   bgcache.render(container);
   renderer.render(realContainer);
 }
 
+function setBrushRotation (rotate) {
+  alterState({ rotation: rotate });
+}
+
 function updateShape (coords) {
-  CIRC_CENTER.x = coords.x - .5;
-  CIRC_CENTER.y = coords.y - .5;
-  if (CIRC_DRAW) {
+  alterState({ cursorX: coords.x - .5 });
+  alterState({ cursorY: coords.y - .5 });
+  if (state.brushPainting) {
     // RADIUS_STATE = Math.sqrt(Math.pow(coords.x - CIRC_CENTER.x, 2) + Math.pow(coords.y - CIRC_CENTER.y, 2));
-    ROT_BY = Math.atan2(coords.y - size.y/2, coords.x - size.x/2);
+    setBrushRotation(Math.atan2(coords.y - size.y/2, coords.x - size.x/2));
   }
 }
 
 function addShape () {
   container.addChild(shapes.sprite);
-  // CIRC_CENTER.x = coords.x;
-  // CIRC_CENTER.y = coords.y;
 }
 
 function commitShape () {
@@ -153,101 +204,24 @@ $('#toolconf-brush-size').on('change mousedown mouseup mousemove', function () {
   }, 0);
 })
 
-$('#toolconf-brush-grid').on('change', function () {
-  if ($(this).prop('checked')) {
+function setGrid (value) {
+  alterState({ gridVisible: value });
+  if (value) {
     realContainer.addChild(gridGraphics);
-    render();
   } else {
     realContainer.removeChild(gridGraphics);
-    render();
   }
-});
+  $('#toolconf-brush-grid').prop('checked', value);
+}
 
-$('#toolconf-brush-alias').on('change', function () {
-  if ($(this).prop('checked')) {
-    ALIAS_STATE = true;
-  } else {
-    ALIAS_STATE = false;
-  }
-});
+function setAlias (value) {
+  alterState({ alias: value });
+  $('#toolconf-brush-alias').prop('checked', value);  
+}
 
-$('#toolconf-color-black').on('click', function () {
-  CIRC_COLOR = '#000000';
-})
-$('#toolconf-color-red').on('click', function () {
-  CIRC_COLOR = '#ff0000';
-})
-$('#toolconf-color-blue').on('click', function () {
-  CIRC_COLOR = '#0000ff';
-})
-$('#toolconf-color-green').on('click', function () {
-  CIRC_COLOR = '#00ff00';
-})
-$('#toolconf-color-yellow').on('click', function () {
-  CIRC_COLOR = '#ffff00';
-})
-$('#toolconf-color-white').on('click', function () {
-  CIRC_COLOR = '#ffffff';
-})
-
-$('#panel-graphics').on('mousewheel', function(event) {
-  if (event.deltaY > 0) {
-    setZoom(Math.min(ZOOM_VALUE + 1, 10));
-  } else if (event.deltaY < 0) {
-    setZoom(Math.max(ZOOM_VALUE - 1, 1));
-  }
-});
-
-window.onkeydown = function (e) {
-  if (e.keyCode == 65) {
-    e.preventDefault();
-    $('#toolconf-brush-alias').prop('checked', !$('#toolconf-brush-alias').prop('checked'));
-    $('#toolconf-brush-alias').trigger('change');
-  }
-  if (e.keyCode == 71) {
-    e.preventDefault();
-    $('#toolconf-brush-grid').prop('checked', !$('#toolconf-brush-grid').prop('checked'));
-    $('#toolconf-brush-grid').trigger('change');
-  }
-  render();
-};
-
-bgcacheSprite.mousedown = function (item) {
-  CIRC_DRAW = true;
-
-  var coords = getCoords(bgcacheSprite, item);
-  
-  // addShape(coords);
-  // updateShape(coords);
-
-  brush(coords);
-  lastCoords = coords;
-
-  render();
-};
-
-window.onmouseup = function (item) {
-  CIRC_DRAW = false;
-  // commitShape();
-  lastCoords = null;
-  render();
-};
-
-bgcacheSprite.mousemove = function (item) {
-  var coords = getCoords(bgcacheSprite, item);
-
-  updateShape(coords);
-
-  if (lastCoords) {
-    var points = calculateLine(lastCoords, coords);
-    lastCoords = coords;
-    points.forEach(function (p) {
-      brush(p);
-    });
-  }
-
-  render();
-};
+function setBrushColor (color) {
+  alterState({ brushColor: color });
+}
 
 function triggerResize () {
   requestAnimationFrame(function () {
@@ -261,13 +235,149 @@ function triggerResize () {
   });
 }
 
-$(window).on('resize', triggerResize)
+function attachListeners () {
+  $('#toolconf-brush-grid').on('change', function () {
+    if ($(this).prop('checked')) {
+      setGrid(true);
+      render();
+    } else {
+      setGrid(false);
+      render();
+    }
+  });
+
+  $('#toolconf-brush-alias').on('change', function () {
+    if ($(this).prop('checked')) {
+      setAlias(true);
+    } else {
+      setAlias(false);
+    }
+  });
+
+  $('#panel-graphics').on('mousewheel', function (event) {
+    if (event.deltaY > 0) {
+      if (event.altKey) {
+        setBrushSize(Math.min(Math.max(state.brushSize - 1, 1), 10));
+      } else {
+        setZoom(Math.min(state.zoom + 1, 10));
+      }
+    } else if (event.deltaY < 0) {
+      if (event.altKey) {
+        setBrushSize(Math.min(Math.max(state.brushSize + 1, 1), 10));
+      } else {
+        setZoom(Math.max(state.zoom - 1, 1));
+      }
+    }
+  });
+
+  window.onkeydown = function (e) {
+    if (e.keyCode == 65) {
+      e.preventDefault();
+      $('#toolconf-brush-alias').prop('checked', !$('#toolconf-brush-alias').prop('checked'));
+      $('#toolconf-brush-alias').trigger('change');
+    }
+    if (e.keyCode == 71) {
+      e.preventDefault();
+      $('#toolconf-brush-grid').prop('checked', !$('#toolconf-brush-grid').prop('checked'));
+      $('#toolconf-brush-grid').trigger('change');
+    }
+    render();
+  };
+
+  bgcacheSprite.mousedown = function (item) {
+    alterState({ brushPainting: true });
+
+    var coords = getCoords(bgcacheSprite, item);
+    
+    // addShape(coords);
+    // updateShape(coords);
+
+    brush(coords);
+    lastCoords = coords;
+
+    render();
+  };
+
+  window.onmouseup = function (item) {
+    alterState({ brushPainting: false });
+    // commitShape();
+    lastCoords = null;
+    // render();
+    saveGraphic();
+  };
+
+  bgcacheSprite.mousemove = function (item) {
+    var coords = getCoords(bgcacheSprite, item);
+
+    updateShape(coords);
+
+    if (lastCoords) {
+      var points = calculateLine(lastCoords, coords);
+      lastCoords = coords;
+      points.forEach(function (p) {
+        brush(p);
+      });
+    }
+
+    render();
+  };
+
+  $(window).on('resize', triggerResize)
+}
 
 
 /**
  * Start
  */
 
-setZoom(ZOOM_VALUE);
-addShape();
-triggerResize();
+function start () {
+  state.brushPainting = false;
+  setGrid(state.gridVisible);
+  setAlias(state.alias);
+  setZoom(state.zoom);
+  setBrushSize(state.brushSize);
+  addShape();
+  triggerResize();
+  attachListeners();
+}
+
+
+function colorPicker (filter) {
+  var OHNO = new PIXI.WebGLRenderer(32, 256);
+  $(OHNO.view).width(32).height(256);
+  var graphics = new PIXI.Graphics();
+  graphics.beginFill(0xFFFF00);
+  // draw a rectangle
+  graphics.drawRect(0, 0, 32, 256);
+  graphics.filters = [filter];
+  graphics.interactive = true;
+  OHNO.render(graphics);
+  return {
+    view: OHNO.view,
+    target: graphics,
+    height: 256,
+  };
+}
+
+;(function () {
+  var h = colorPicker(new HFilter);
+  var s = colorPicker(new SFilter);
+  var b = colorPicker(new BFilter);
+
+  h.target.mouseup = function (event) {
+    var coords = getCoords(h.target, event);
+    var hval = coords.y / h.height;
+
+    var color = tinycolor({ h: hval*255, s: 255, v: 255 }).toRgb();
+    var brushColor = '#' + ('000000' + ((color.r << 16) + (color.g << 8) + color.b).toString(16)).slice(-6);
+    alterState({ brushColor: brushColor })
+    console.log(brushColor);
+  }
+
+  $('#ohno')
+    .append(h.view)
+    .append(s.view)
+    .append(b.view);
+})();
+
+
